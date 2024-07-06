2396a3258a4364a0a29eaf520a82f027f7dd1fee
ValAn <valovaanna04@mail.ru>
2024-07-06 23:22:36 +0400

    first commit

---
+++ slider.js
@@ -0,0 +1,10 @@
+const cardsSwiper = new Swiper(".js-swiper", {
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".js-arrow-prev",
+      prevEl: ".js-arrow-next",
+    },
+    centerMode: true
+  });
